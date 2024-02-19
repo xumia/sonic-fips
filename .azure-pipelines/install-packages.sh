493530e43d1f78bd6a37a20b7fa7c0131944ea58
xumia <xumia@microsoft.com>
2024-02-19 08:54:45 +0000

    Upgrade SymCrypt to 103.4.1 to support the armhf

---
+++ .azure-pipelines/install-packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+sudo mkdir -p $HOME
+sudo chown $USER $HOME
+sudo apt-get update
+sudo apt-get install -y git python3-pip
+sudo apt-get install -y cmake git make build-essential quilt debhelper bc python3 python3-pip sudo libssl-dev libgcc-10-dev
+sudo apt-get install -y clang
+sudo apt-get install -y openssl libssl-dev libssl1.1
+sudo apt-get install -y dh-exec dh-runit libaudit-dev libedit-dev libfido2-dev libgtk-3-dev libkrb5-dev
+sudo apt-get install -y libwrap-dev pkg-config
+sudo apt-get install -y libpam-dev libselinux1-dev libsystemd-dev libwrap0-dev
+[ -f  src/SymCrypt/scripts/requirements.txt ] && sudo pip3 install -r src/SymCrypt/scripts/requirements.txt
+if [ "$ARCH" == "armhf" ]; then
+    sudo apt-get install -y libc6-dev-armhf-cross
+    sudo ln -s /usr/include/arm-linux-gnueabihf/openssl/opensslconf.h /usr/include/openssl/opensslconf.h
+fi
